3aba92785b12ab089b4d7feaefbf1916f7386260
pradeepmalvi <p.malviyaaa@gmail.com>
2021-04-10 02:08:38 +0530

    Front-end UI updates and basic server started with express

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
-  return (
-    <div>
-      hello
-    </div>
-  );
+	return (
+		<Router>
+			<Header></Header>
+			<main className="py-3">
+				<Container>
+					<Route path="/" component={HomeScreen} exact />
+					<Route path="/product/:id" component={ProductScreen} />
+				</Container>
+			</main>
+			<Footer></Footer>
+		</Router>
+	);
 }
 
 export default App;
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import Product from '../components/Product';
+import { Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+
+const HomeScreen = () => {
+	const [products, setProducts] = useState([]);
+	useEffect(() => {
+		const getProducts = async () => {
+			const { data } = await axios.get('/api/products');
+			setProducts(data);
+		};
+		getProducts();
+	}, []);
+	return (
+		<>
+			<h1>Latest Products</h1>
+			<Row>
+				{products.map((product) => (
+					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+						<Product product={product}></Product>
+					</Col>
+				))}
+			</Row>
+		</>
+	);
+};
+
+export default HomeScreen;
