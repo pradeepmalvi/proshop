928fca94e0b3f6ffdda5a076ee71e89011202aa3
pradeepmalvi <p.malviyaaa@gmail.com>
2021-04-11 01:23:06 +0530

    Cart CRUD update

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,21 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducers';
 
 const reducer = combineReducers({
 	productList: productListReducer,
 	productDetails: productDetailsReducer,
+	cart: cartReducer,
 });
 
-let initialState = {};
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
+
+let initialState = {
+	cart: {
+		cartItems: cartItemsFromStorage,
+	},
+};
 
 const middleware = [thunk];
 
