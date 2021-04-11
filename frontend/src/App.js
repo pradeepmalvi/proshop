4e229f4218354d496e53994e773f1d5a859a8dc8
pradeepmalvi <p.malviyaaa@gmail.com>
2021-04-12 00:53:45 +0530

    User authentication and user profile are done in both side

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import { Container } from 'react-bootstrap';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 function App() {
 	return (
@@ -15,6 +18,9 @@ function App() {
 					<Route path="/" component={HomeScreen} exact />
 					<Route path="/product/:id" component={ProductScreen} />
 					<Route path="/cart/:id?" component={CartScreen} />
+					<Route path="/login" component={LoginScreen} />
+					<Route path="/register" component={RegisterScreen} />
+					<Route path="/profile" component={ProfileScreen} />
 				</Container>
 			</main>
 			<Footer></Footer>
