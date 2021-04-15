a87611671e828d4216d60574849783fe8aa36c22
pradeepmalvi <p.malviyaaa@gmail.com>
2021-04-15 10:57:43 +0530

    Order details updates on both side

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,6 +1,13 @@
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS } from '../constants/orderConstants';
+import {
+	ORDER_CREATE_FAIL,
+	ORDER_CREATE_REQUEST,
+	ORDER_CREATE_SUCCESS,
+	ORDER_DETAILS_FAIL,
+	ORDER_DETAILS_REQUEST,
+	ORDER_DETAILS_SUCCESS,
+} from '../constants/orderConstants';
 
-export const orderCreateReducer = () => (state = {}, action) => {
+export const orderCreateReducer = (state = {}, action) => {
 	switch (action.type) {
 		case ORDER_CREATE_REQUEST:
 			return {
@@ -14,7 +21,30 @@ export const orderCreateReducer = () => (state = {}, action) => {
 			};
 		case ORDER_CREATE_FAIL:
 			return {
-                loading:false,
+				loading: false,
+				error: action.payload,
+			};
+
+		default:
+			return state;
+	}
+};
+
+export const orderDetailsReducer = (state = { loading: true, orderItems: [], shippingAddress: {} }, action) => {
+	switch (action.type) {
+		case ORDER_DETAILS_REQUEST:
+			return {
+				...state,
+				loading: true,
+			};
+		case ORDER_DETAILS_SUCCESS:
+			return {
+				loading: false,
+				order: action.payload,
+			};
+		case ORDER_DETAILS_FAIL:
+			return {
+				loading: false,
 				error: action.payload,
 			};
 
